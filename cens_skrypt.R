f3a3bf95965c9f75b10b13524281f62abfbeecfd
kurkachatka <marta.witkowskaa@gmail.com>
2024-11-08 11:06:32 +0100

    uploading Rskripts for the class

---
+++ cens_skrypt.R
@@ -0,0 +1,114 @@
+
+
+# install.packages('tidyverse')
+# install.packages('readxl')
+# install.packages('factoextra')
+# install.packages('gridExtra')
+
+library(tidyverse)
+library(readxl)
+library(gridExtra)
+library(factoextra)
+
+
+# ustalanie working directory 
+# cens <- read_excel("C:/Users/MartaWitkowska/OneDrive - University of Gdansk/Dydaktyka/Ekologia zwierz¹t/Biologia/Census_MainDB.xlsx")
+
+
+# przygotowanie danych ze zliczonymi gatunkami
+
+n_gat <- cens %>% 
+  group_by(GRUPA, PUNKT) %>% 
+  summarise(N_GAT=n(),
+            AUTA = median(AUTA),
+            PIESI = median(PIESI),
+            BUD = median(BUD),
+            DRZEW = median(DRZEW),
+            WODA = median(WODA))
+
+n_gat <- as.data.frame(n_gat)
+
+
+# przygotowanie danych do PCA, tylko ze zmiennych œrodowiskowych
+
+db_pca <- n_gat %>% select(!GRUPA & !PUNKT & !N_GAT)
+
+# obliczenie PCA
+
+wyniki.pca <- prcomp(db_pca, scale = T)
+
+
+# wizualizacja wariancji objasnionej przez kazde PC
+
+fviz_eig(wyniki.pca)
+
+# biplot
+
+fviz_pca_var(wyniki.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE)
+
+
+# obliczenie wartosci PC dla kazdego punktu obserwacyjnego
+
+
+kalk_pca <- predict(wyniki.pca, newdata = n_gat)
+
+n_gat_pca <- bind_cols(n_gat, kalk_pca)
+
+
+# zmiany bioroznorodnosci w zaleznosci od cech srodowiska zurbanizowanego
+
+p1 <- n_gat_pca %>% 
+  ggplot(aes(y = N_GAT, x = PC1)) + 
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_classic()
+
+
+p2 <- n_gat_pca %>% 
+  ggplot(aes(y = N_GAT, x = AUTA)) + 
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_classic()
+
+p3 <- n_gat_pca %>% 
+  ggplot(aes(y = N_GAT, x = PIESI)) + 
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_classic()
+
+p4 <- n_gat_pca %>% 
+  ggplot(aes(y = N_GAT, x = DRZEW)) + 
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_classic()
+
+
+p5 <- n_gat_pca %>% 
+  ggplot(aes(y = N_GAT, x = WODA)) + 
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_classic()
+
+
+p_s <- grid.arrange(p2, p3, p4, p5)
+
+grid.arrange(p1, p_s, ncol = 2)
+
+
+
+
+
+cor.test(n_gat_pca$N_GAT, n_gat_pca$PC1, method = 'spearman')
+
+cor.test(n_gat_pca$N_GAT, n_gat_pca$AUTA, method = 'spearman')
+
+cor.test(n_gat_pca$N_GAT, n_gat_pca$PIESI, method = 'spearman')
+
+cor.test(n_gat_pca$N_GAT, n_gat_pca$DRZEW, method = 'spearman')
+
+cor.test(n_gat_pca$N_GAT, n_gat_pca$WODA, method = 'spearman')
+
+
---
+++ hier_script.R
@@ -0,0 +1,133 @@
+# 
+# install.packages('tidyverse')
+# install.packages('readxl')
+# install.packages('gridExtra')
+
+library(tidyverse)
+library(readxl)
+library(gridExtra)
+
+# wczytaj plik
+# hier <- read_excel('C:/Users/marta/OneDrive - University of Gdansk/Dydaktyka/Ekologia zwierz¹t/Biologia/Park Oliwski/Hier_MainDB.xlsx')
+
+
+#sumowanie agresji
+
+hier <- hier %>% mutate(AGR = AGR_F + AGR_M)
+
+# obliczanie % zachowan uleglych
+
+
+hier <- hier %>% mutate(ULE = ULE_F + ULE_M) %>% 
+  mutate(ALL = ULE+AGR) %>% 
+  mutate(procULE = ULE/ALL)
+
+
+# obliczanie przecietnej pozycji (na podstawie mediany)
+
+hier <- hier %>% mutate(POZ = apply(select(., POZ1, POZ2, POZ3, POZ4, POZ5),
+                   1, median, na.rm = TRUE))
+
+hier$POZ <-  as.integer(hier$POZ) # zmiana na liczby calkowite
+
+
+# Porownanie plci pod wzgledem liczby zachowan agresywnych
+
+hier %>% 
+  filter(!is.na(Plec)) %>% # odfiltrowanie obserwacji bez plci
+  ggplot(aes(x = Plec, y = AGR)) +
+  geom_boxplot() +
+  theme_classic()
+
+
+
+# Porownanie plci pod wzgledem procentu zachowan uleglych
+
+
+hier %>% 
+  filter(!is.na(Plec)) %>% # odfiltrowanie obserwacji bez plci
+  ggplot(aes(x = Plec, y = procULE)) +
+  geom_boxplot() +
+  theme_classic()
+
+
+# to samo, z uwzglednieniem plci odbiorcy
+
+
+f1 <- hier %>% 
+  filter(!is.na(Plec)) %>% # odfiltrowanie obserwacji bez plci
+  ggplot(aes(x = Plec, y = AGR_M)) +
+  geom_boxplot() +
+  ylim(-1, 30) +
+  theme_classic()
+
+f2 <- hier %>% 
+  filter(!is.na(Plec)) %>% # odfiltrowanie obserwacji bez plci
+  ggplot(aes(x = Plec, y = AGR_F)) +
+  geom_boxplot() +
+  ylim(-1, 30) +
+  theme_classic()
+
+grid.arrange(f1, f2, ncol = 2)
+
+
+# podsumowanie statystyk opisowych
+
+podsum1 <- hier %>% 
+  filter(!is.na(Plec)) %>% 
+  group_by(Plec) %>% 
+  summarise(mediana.AGR = median(AGR, na.rm = F),
+            roz.kwar.AGR = IQR(AGR, na.rm = F),
+            mediana.AGR_M = median(AGR_M, na.rm = F),
+            roz.kwar.AGR_M = IQR(AGR_M, na.rm = F),
+            mediana.AGR_F = median(AGR_F, na.rm = F),
+            roz.kwar.AGR_F = IQR(AGR_F, na.rm = F),
+            mediana.procULE = median(procULE, na.rm = F),
+            roz.kwar.procULE = IQR(procULE, na.rm = F))
+
+
+View(podsum1)
+
+
+# test Wilcoxona
+
+wilcox.test(data=hier, AGR ~ Plec)
+wilcox.test(data=hier, AGR_M ~ Plec)
+wilcox.test(data=hier, AGR_F ~ Plec)
+wilcox.test(data=hier, procULE ~ Plec)
+
+
+
+# agresja a jedzenie
+
+hier %>% 
+  ggplot(aes(y = ZJAD, x = AGR)) +
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_classic()
+
+
+cor.test(hier$ZJAD, hier$AGR, method = 'spearman')
+
+
+
+hier %>% 
+  filter(!is.na(Proba)) %>% 
+  ggplot(aes(y = AGR, x = Proba)) +
+  geom_boxplot() +
+  theme_classic()
+
+
+
+# Pozycja a agresja
+
+
+hier %>% 
+  filter(!is.na(POZ)) %>% 
+  ggplot(aes(x = as.factor(POZ), y = AGR)) +
+  geom_boxplot() +
+  theme_classic()
+
+
+kruskal.test(data = hier, AGR~as.factor(POZ))
+
