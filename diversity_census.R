0dacf433d1b2945124f270fc9bfe6b35d3afdde0
kurkachatka <marta.witkowskaa@gmail.com>
2025-11-13 18:22:47 +0100

    getting the taxonomix diversity plots

--- diversity_census.R
+++ diversity_census.R
@@ -3,4 +3,113 @@ library(tidyverse)
 setwd("C:/Users/marta/OneDrive - University of Gdansk/Dydaktyka/Ekologia zwierząt/Animal-Ecology-Lab")
 
 
-div.data <- read_excel
+div.data <- read_excel('DiversityDB1.xlsx', sheet = 'Census')
+
+str(div.data)
+
+
+# przygotowanie danych ze zliczonymi gatunkami
+
+n_spec <- div.data %>% 
+  group_by(GRUP, POINT) %>% 
+  summarise(N_spec=n(),
+            VEGETATION = mean(VEGETATION),
+            WATER = mean(WATER),
+            SEALED = mean(SEALED))
+
+n_spec <- as.data.frame(n_spec)
+
+
+n_spec %>% 
+  ggplot(aes(y = N_spec, x = VEGETATION)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_spec %>% 
+  ggplot(aes(y = N_spec, x = WATER)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_spec %>% 
+  ggplot(aes(y = N_spec, x = SEALED)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+
+# przygotowanie danych ze zliczonymi rodzinami
+
+n_fam <- div.data %>% 
+  group_by(GRUP, POINT) %>% 
+  summarise(N_fam=n_distinct(FAMILY),
+            VEGETATION = mean(VEGETATION),
+            WATER = mean(WATER),
+            SEALED = mean(SEALED))
+
+n_fam <- as.data.frame(n_fam)
+
+
+n_fam %>% ggplot(aes(N_fam, x = VEGETATION)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_fam %>% ggplot(aes(N_fam, x = WATER)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+
+# przygotowanie danych ze zliczonymi rzędami
+
+n_ord <- div.data %>% 
+  group_by(GRUP, POINT) %>% 
+  summarise(N_ord=n_distinct(ORDER),
+            VEGETATION = mean(VEGETATION),
+            WATER = mean(WATER),
+            SEALED = mean(SEALED))
+
+
+n_ord <- as.data.frame(n_ord)
+
+
+n_ord %>% ggplot(aes(N_ord, x = VEGETATION)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_ord %>% ggplot(aes(N_ord, x = WATER)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+# przygotowanie danych ze zliczonymi rzędami
+
+n_ord <- div.data %>% 
+  group_by(GRUP, POINT) %>% 
+  summarise(N_ord=n_distinct(ORDER),
+            VEGETATION = mean(VEGETATION),
+            WATER = mean(WATER),
+            SEALED = mean(SEALED))
+
+
+n_ord <- as.data.frame(n_ord)
+
+
+n_ord %>% ggplot(aes(N_ord, x = VEGETATION)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_ord %>% ggplot(aes(N_ord, x = WATER)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+
+
