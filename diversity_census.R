f248c16dbce27c47c36cdabca2faa232a6c6754c
kurkachatka <marta.witkowskaa@gmail.com>
2025-11-13 18:29:15 +0100

    diversity on the functional level

--- diversity_census.R
+++ diversity_census.R
@@ -86,30 +86,70 @@ n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
   geom_point() +
   geom_smooth(method = 'lm')
 
-# przygotowanie danych ze zliczonymi rzędami
 
-n_ord <- div.data %>% 
+
+# przygotowanie danych ze zliczonymi grupami pokarmowymi
+
+n_food <- div.data %>% 
   group_by(GRUP, POINT) %>% 
-  summarise(N_ord=n_distinct(ORDER),
+  summarise(N_food = n_distinct(FOOD),
             VEGETATION = mean(VEGETATION),
             WATER = mean(WATER),
             SEALED = mean(SEALED))
 
 
-n_ord <- as.data.frame(n_ord)
+n_food <- as.data.frame(n_food)
 
 
-n_ord %>% ggplot(aes(N_ord, x = VEGETATION)) +
+n_food %>% ggplot(aes(N_food, x = VEGETATION)) +
   geom_point() +
   geom_smooth(method = 'lm')
 
-n_ord %>% ggplot(aes(N_ord, x = WATER)) +
+n_food %>% ggplot(aes(N_food, x = WATER)) +
   geom_point() +
   geom_smooth(method = 'lm')
 
-n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
+n_food %>% ggplot(aes(N_food, x = SEALED)) +
   geom_point() +
   geom_smooth(method = 'lm')
+
+
+
+# przygotowanie danych ze zliczonymi grupami behawioralnymi
+
+n_behav <- div.data %>% 
+  group_by(GRUP, POINT) %>% 
+  summarise(N_behav = n_distinct(BEHAV),
+            VEGETATION = mean(VEGETATION),
+            WATER = mean(WATER),
+            SEALED = mean(SEALED))
+
+
+n_behav <- as.data.frame(n_behav)
+
+
+n_behav %>% ggplot(aes(N_behav, x = VEGETATION)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_behav %>% ggplot(aes(N_behav, x = WATER)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+n_behav %>% ggplot(aes(N_behav, x = SEALED)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+
+
+# porowanie grup takosnomicznych
+
+
+
+
+
+
+
 
 
 
