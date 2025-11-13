4db89979e3adce1c1b68a62651945772c22c2ed2
kurkachatka <marta.witkowskaa@gmail.com>
2025-11-13 20:19:56 +0100

    correlation for number of species

--- diversity_census.R
+++ diversity_census.R
@@ -1,18 +1,29 @@
 library(readxl)
 library(tidyverse)
+library(gridExtra)
 setwd("C:/Users/marta/OneDrive - University of Gdansk/Dydaktyka/Ekologia zwierząt/Animal-Ecology-Lab")
 
 
-div.data <- read_excel('DiversityDB1.xlsx', sheet = 'Census')
+div.data1 <- read_excel('DiversityDB1.xlsx', sheet = 'Census')
+div.data2 <- read_excel('DiversityDB2.xlsx', sheet = 'Census')
 
-str(div.data)
+
+
+
+div.data1 <- div.data1 %>% mutate(VEGETATION = VEGETATION/100,
+                                WATER = WATER/100,
+                                SEALED = SEALED/100)
+
+
+div.data <-  bind_rows(div.data1, div.data2)
 
 
 # przygotowanie danych ze zliczonymi gatunkami
 
+# stwórz dane zliczające gatunki dla kazdego punktu
 n_spec <- div.data %>% 
   group_by(GRUP, POINT) %>% 
-  summarise(N_spec=n(),
+  summarise(N_spec=n_distinct(SPECIES),
             VEGETATION = mean(VEGETATION),
             WATER = mean(WATER),
             SEALED = mean(SEALED))
@@ -20,23 +31,60 @@ n_spec <- div.data %>%
 n_spec <- as.data.frame(n_spec)
 
 
-n_spec %>% 
+# Wizualizacja
+
+spec_plot1 <- n_spec %>% 
   ggplot(aes(y = N_spec, x = VEGETATION)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
+  geom_point(size = 6,alpha = 0.7, position = position_jitter()) +
+  geom_smooth(method = 'lm', color = 'red') +
+  theme_classic() +
+  theme(axis.title = element_text(size = 15),
+        axis.text = element_text(size = 12),
+        plot.tag = element_text(size = 20)) +
+  labs(y = 'Liczba gatunków', x = 'Indeks roślinności',
+       tag = 'A')
 
-n_spec %>% 
+
+spec_plot2 <- n_spec %>% 
   ggplot(aes(y = N_spec, x = WATER)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
+  geom_point(size = 6,alpha = 0.7, position = position_jitter(width = 0.01)) +
+  geom_smooth(method = 'lm', color = 'red') +
+  theme_classic() +
+  theme(axis.title = element_text(size = 15),
+        axis.text = element_text(size = 12),
+        plot.tag = element_text(size = 20)) +
+  labs(y = 'Liczba gatunków', x = 'Indeks dostępności wody',
+       tag = 'B')
 
-n_spec %>% 
+
+spec_plot3 <- n_spec %>% 
   ggplot(aes(y = N_spec, x = SEALED)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
+  geom_point(size = 6,alpha = 0.7, position = position_jitter()) +
+  geom_smooth(method = 'lm', color = 'red') +
+  theme_classic() +
+  theme(axis.title = element_text(size = 15),
+        axis.text = element_text(size = 12),
+        plot.tag = element_text(size = 20)) +
+  labs(y = 'Liczba gatunków', x = 'Indeks powierzchni uszczelnionej',
+       tag = 'C')
+
+
+grid.arrange(spec_plot1, spec_plot2, spec_plot3, ncol = 3)
+
+
+# analiza korelacji Spearmana (ze wzgledu na brak normalnosci rozkladu zmiennych)
+
+cor.test(n_spec$N_spec, n_spec$VEGETATION, method = 'spearman')
+
+cor.test(n_spec$N_spec, n_spec$WATER, method = 'spearman')
+
+cor.test(n_spec$N_spec, n_spec$SEALED, method = 'spearman')
+
 
 
 # przygotowanie danych ze zliczonymi rodzinami
+
+
 
 n_fam <- div.data %>% 
   group_by(GRUP, POINT) %>% 
@@ -47,15 +95,27 @@ n_fam <- div.data %>%
 
 n_fam <- as.data.frame(n_fam)
 
+# Wizualizacja
 
-n_fam %>% ggplot(aes(N_fam, x = VEGETATION)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
+fam_plot1 <- n_fam %>% ggplot(aes(N_fam, x = VEGETATION)) +
+  geom_point(size = 6,alpha = 0.7, position = position_jitter()) +
+  geom_smooth(method = 'lm', color = 'seagreen', size = 2) +
+  theme_classic() +
+  theme(axis.title = element_text(size = 15),
+        axis.text = element_text(size = 12),
+        plot.tag = element_text(size = 20)) +
+  labs(y = 'Liczba rodzin', x = 'Indeks roślinności',
+       tag = 'A')
 
-n_fam %>% ggplot(aes(N_fam, x = WATER)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
-
+fam_plot2 <- n_fam %>% ggplot(aes(N_fam, x = WATER)) +
+  geom_point(size = 6,alpha = 0.7, position = position_jitter()) +
+  geom_smooth(method = 'lm', color = 'seagreen', size = 2) +
+  theme_classic() +
+  theme(axis.title = element_text(size = 15),
+        axis.text = element_text(size = 12),
+        plot.tag = element_text(size = 20)) +
+  labs(y = 'Liczba rodzin', x = 'Indeks dostępności wody',
+       tag = 'B')
 n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
   geom_point() +
   geom_smooth(method = 'lm')
@@ -82,7 +142,7 @@ n_ord %>% ggplot(aes(N_ord, x = WATER)) +
   geom_point() +
   geom_smooth(method = 'lm')
 
-n_fam %>% ggplot(aes(N_fam, x = SEALED)) +
+n_ord %>% ggplot(aes(N_ord, x = SEALED)) +
   geom_point() +
   geom_smooth(method = 'lm')
 
